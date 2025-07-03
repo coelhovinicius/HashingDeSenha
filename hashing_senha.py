f8ad5d4e9c2682080d7141aac69529f3b60de065
coelhovinicius <coelhovinicius@yahoo.com.br>
2025-07-03 12:39:10 -0300

    Hashing de Senha

--- hashing_senha.py
+++ hashing_senha.py
@@ -1,20 +1,22 @@
+''' Criação de Hash de senha '''
+
 import hashlib
 
+
 def gerar_hash(senha):
-    # Convertemos a string para bytes
+    ''' Convertemos a string para bytes '''
     senha_bytes = senha.encode('utf-8')
-    
+
     # Criamos o hash usando SHA-256
     hash_obj = hashlib.sha256(senha_bytes)
-    
+
     # Retornamos o hash no formato hexadecimal
     return hash_obj.hexdigest()
 
+
 # Exemplo de uso
-senha = "minhaSenha123"
-hash_resultado = gerar_hash(senha)
+SENHA = "minhaSenha123"
+HASH_RESULTADO = gerar_hash(SENHA)
 
-print(f"Senha original: {senha}")
-print(f"Hash gerado: {hash_resultado}")
-
-   
+print(f"Senha original: {SENHA}")
+print(f"Hash gerado: {HASH_RESULTADO}")
